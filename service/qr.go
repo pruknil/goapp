300e6f0dd938d35041683f694524d6f7a4aa2960
pruknil <pruknil@gmail.com>
2019-12-22 14:57:52 +0700

    add mock email

--- service/qr.go
+++ service/qr.go
@@ -3,34 +3,41 @@ package service
 import (
 	"encoding/base64"
 	"encoding/json"
-	"fmt"
-	"github.com/pruknil/goapp/backends/http"
 	"github.com/skip2/go-qrcode"
 )
 
 type QRService struct {
 	baseService
-	backendReq http.DopaReq
-	backendRes *http.DopaRes
+	frontendReq QRServiceReq
+	frontendRes QRServiceRes
+	//backendReq http.DopaReq
+	//backendRes *http.DopaRes
+}
+
+type QRServiceReq struct {
+	Input string `json:"input"`
+}
+type QRServiceRes struct {
+	Base64Str string `json:"base64Str"`
 }
 
 func (s *QRService) OutputMapping() error {
 	s.Response = ResMsg{
 		Header: ResHeader{},
-		Body:   s.backendRes,
+		Body:   s.frontendRes,
 	}
 	return nil
 }
 
 func (s *QRService) InputMapping() error {
 	jsonString, _ := json.Marshal(s.Request.Body)
-	json.Unmarshal(jsonString, &s.backendReq)
+	json.Unmarshal(jsonString, &s.frontendReq)
 	return nil
 }
 
 func (s *QRService) Business() error {
 	var png []byte
-	q, err := qrcode.New("https://www.google.co.th", qrcode.Low)
+	q, err := qrcode.New(s.frontendReq.Input, qrcode.Low)
 	if err != nil {
 		panic(err)
 	}
@@ -40,6 +47,7 @@ func (s *QRService) Business() error {
 		panic(err)
 	}
 	imgBase64Str := base64.StdEncoding.EncodeToString(png)
-	fmt.Println(imgBase64Str)
+	//fmt.Println(imgBase64Str)
+	s.frontendRes.Base64Str = imgBase64Str
 	return nil
 }

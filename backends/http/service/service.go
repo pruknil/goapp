734374ff14d3a0d687a4db20cc3ab4e92ffe2443
K0963167 <pruk.ni@kbtg.tech>
2020-04-21 10:42:00 +0700

    jjjrerr

--- backends/http/service/service.go
+++ backends/http/service/service.go
@@ -17,12 +17,19 @@ func New(service http.IHttpBackendService) IHttpBackend {
 }
 
 func (s *HttpBackendService) AirQuality() (*http.AQIRes, error) {
+	q := url.Values{}
+	q.Set("bbox", "100.1972940089172,13.47902387099105,100.78038479108272,13.979292336476917")
+	q.Set("units.temperature", "celsius")
+	q.Set("units.distance", "kilometer")
+	q.Set("AQI", "US")
+	q.Set("language", "th")
 	header := make(map[string][]string)
 	header["Authorization"] = []string{"Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsIng1dCI6IkJCOENlRlZxeWFHckdOdWVoSklpTDRkZmp6dyIsImtpZCI6IkJCOENlRlZxeWFHckdOdWVoSklpTDRkZmp6dyJ9.eyJhdWQiOiJodHRwczovL2thc2lrb3JuYmFua2dyb3VwLmNvbS84ZmZmN2RjNi0yMTQ3LTQ0NWEtYjcwYy1kM2I3NTFiODlmYzIiLCJpc3MiOiJodHRwczovL3N0cy53aW5kb3dzLm5ldC84ZTExZGY5Zi00NjE1LTQzNGYtYTZjNi04YTBjYjRmZmViNmMvIiwiaWF0IjoxNTczNTU2MjM0LCJuYmYiOjE1NzM1NTYyMzQsImV4cCI6MTU3MzU2MDEzNCwiYWNyIjoiMSIsImFpbyI6IjQyVmdZTmhmcWxnNE1jTEFtdWtSay91YkJUNnZqdnZxYVppdUw0bnVPbk43V3RGTGsyMEEiLCJhbXIiOlsicHdkIl0sImFwcGlkIjoiYzUxNWFiMTItZDMzYy00MWEwLWFjMzEtNGMzZjUxN2RkYjUyIiwiYXBwaWRhY3IiOiIwIiwiZmFtaWx5X25hbWUiOiJOaWxzdXJpeWFrb24iLCJnaXZlbl9uYW1lIjoiUHJ1ayIsImlwYWRkciI6IjQ5LjIzMS4yMTIuMTQ4IiwibmFtZSI6IlBydWsgTmlsc3VyaXlha29uIiwib2lkIjoiMDRkNTJjOTctODM5Yy00MTI0LWI3NzMtYmY5OGFjYzdlODNjIiwib25wcmVtX3NpZCI6IlMtMS01LTIxLTM2ODgwOTM4NjItNDA4MjI2OTg0LTM1MTAwMzEyOTktMjMzMzYwIiwic2NwIjoidXNlcl9pbXBlcnNvbmF0aW9uIiwic3ViIjoibzdXR2JYelNmVW9JNzFGMG5VeGk3cU1ESGpNOWo2MlNZdUNkci1tVnJkNCIsInRpZCI6IjhlMTFkZjlmLTQ2MTUtNDM0Zi1hNmM2LThhMGNiNGZmZWI2YyIsInVuaXF1ZV9uYW1lIjoicHJ1ay5uaUBrYnRnLnRlY2giLCJ1cG4iOiJwcnVrLm5pQGtidGcudGVjaCIsInV0aSI6IlE5TVEyUDJxWUUtSEtLVHRmcWJzQUEiLCJ2ZXIiOiIxLjAifQ.jZhdln3qkS0l_VOxbf-LQ-cfgg0y_35-xYR6w_Ril286XXy-pO6EKQJJLUF0qT6qcRWmLU_drCRsdvCryzqmKR_BpsvnbZXcnW3GhJWJMg2BD_82ynUdYG4DYrFd6nX9upmasYqPX4qpVORkkRjKyKtiUq7RK64P6CJIBxzUqiCEEYG1tuOxRjnY0PJWMfXHAvglPWDGU2XiHapSkxbQf3utcubSFE2tCYyuLBs8YVDWiNJMVpYKZ6amWXCzbXucGwxe3XIJFu-3eMDTt-25nVUiBUZX0GQCvKDV93fFfWSx7R6sxi8vzAlhLgC5FfHO5iCFxH8nTpYzG6VRKrclLw"}
 	header["Content-Type"] = []string{"application/x-www-form-urlencoded; charset=UTF-8"}
 	req := http.Req{
 		Method: "GET",
-		Url:    "https://website-api.airvisual.com/v1/places/map?bbox=100.1972940089172,13.47902387099105,100.78038479108272,13.979292336476917&units.temperature=celsius&units.distance=kilometer&AQI=US&language=th",
+		Url:    "https://website-api.airvisual.com/v1/places/map?" + q.Encode(),
+		Header: header,
 	}
 	byteRs, err := s.IHttpBackendService.DoRequest(req)
 	if err != nil {
